9f35b750d7bb20d0c6cd6b1461cb99ddd8a45d47
diniztsell <165039650+diniztsell@users.noreply.github.com>
2024-05-09 11:26:11 -0300

    lulu

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const textos = document.querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
-      for (let j = 0; j < botoes.length; j++) {
-          botoes[j].classList.remove("ativo");
-          textos[j].classList.remove("ativo");
-       }
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
 
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
@@ -42,12 +42,12 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-     for (let i=0; i<contadores.length;i++){
+    for (let i=0; i<contadores.length;i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
         document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
-   }
+    }
 }
 
 function comecaCronometro(){
